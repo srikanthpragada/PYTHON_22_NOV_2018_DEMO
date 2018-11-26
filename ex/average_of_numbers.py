d66ee9a0dda0ab779ce45823b1020fbaccf55af5
srikanthpragada <srikanthpragada@gmail.com>
2018-11-26 21:00:55 +0530

    26-NOV

---
+++ ex/average_of_numbers.py
@@ -0,0 +1,16 @@
+total = 0
+count = 0
+
+while True:
+    num = int(input("Enter a number [0 to stop] :"))
+    if num == 0:
+        break
+
+    total += num
+    count += 1
+
+print("Average = ", total / count)
+
+
+
+
