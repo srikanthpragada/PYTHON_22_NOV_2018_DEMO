b6984ef2e7b8dd9fe25aa126a399eae90b40e9d6
srikanthpragada <srikanthpragada@gmail.com>
2018-11-27 21:02:56 +0530

    27-NOV

---
+++ ds/sort_numbers.py
@@ -0,0 +1,13 @@
+# Take numbers from user and print in sorted order
+
+nums = []
+
+while True:
+    num = int(input("Enter a number [0 to stop] :"))
+    if num == 0:
+        break
+
+    nums.append(num)
+
+for n in sorted(nums):
+    print(n, end=' ')
