fc464ab274d1f8b3618682096563d373199a1ff9
srikanthpragada <srikanthpragada@gmail.com>
2018-12-04 21:01:36 +0530

    04-DEC

--- funs/simple.py
+++ funs/simple.py
@@ -1,7 +1,31 @@
-
 def fun():
     print("In fun()")
 
 
-fun()
+def add(a, b=0):
+    return a + b
 
+
+def fun2(*, n1=1, n2):  # keyword only arguments
+    pass
+
+
+def fun3(*names, msg="Hello"):
+    for n in names:
+        print(msg, n)
+
+
+def fun4(a=10, *b):
+    print(a)
+    print(b)
+
+
+def kwargs(n1, n2, *args):
+    print(n1,n2)
+    print(args)
+
+
+kwargs(10,20)
+
+# fun3("abc", 'pqr', msg="Good Evening")
+# fun4(10,20,30)
