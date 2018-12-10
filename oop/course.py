7717db83e93fd946e9cb6a0f03fc75a86a5e482e
srikanthpragada <srikanthpragada@gmail.com>
2018-12-10 20:59:44 +0530

    10-DEC

--- oop/course.py
+++ oop/course.py
@@ -1,22 +1,45 @@
 class Course:
+    # Class attributes
+    taxper = 15
+
+    @staticmethod
+    def change_taxper(newtaxper):
+        Course.taxper = newtaxper
+
     # Constructor
     def __init__(self, name, duration=None, f=None):
         # Object attributes
         self.name = name
-        self.__duration = duration
-        self.__fee = f
+        self.duration = duration
+        self.fee = f
 
+    def get_fee(self):
+        return  self.fee + self.fee * Course.taxper / 100
+
+    # Special method
     def __str__(self):
-        return  f"{self.name} - {self.__duration} - {self.__fee}"
+        return  f"{self.name} - {self.duration} - {self.fee}"
+
+    def __eq__(self,other):
+        print("__eq__")
+        return self.name == other.name and self.duration == other.duration  and self.fee == other.fee
+
+    def __gt__(self, other):
+        return self.fee > other.fee
 
 
-c1 = Course("Python", 40, 4000)   # Create object
-print(str(c1))  # c1.__str__()
-# print(c1.name,c1.__duration)
+c1 = Course("Python", 40,4000)   # Create object
+Course.change_taxper(20)
+print(c1.get_fee())
 
 
-c2 = Course("Data Science", f=10000)
-print(c2)
+
+
+
+
+
+
+
 
 
 
