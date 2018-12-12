9e7e02480d72d267d3b2abefb635cab49f723d13
srikanthpragada <srikanthpragada@gmail.com>
2018-12-12 20:59:03 +0530

    12-DEC

---
+++ oop/ex_demo.py
@@ -0,0 +1,9 @@
+try:
+    num = int(input("Enter number :"))
+    print(100 / num)
+except:
+    print("Invalid input")
+else:
+    print("Done")
+
+
--- oop/minh.py
+++ oop/minh.py
@@ -1,25 +1,25 @@
-class A:
+class Common:
     def print(self):
-        print("In print of A")
+        print("In print of Common")
+
+
+class A(Common):
+    # def print(self):
+    #     print("In print of A")
 
     def process(self):
         print("In Process of A")
 
 
-class B:
+class B(Common):
     def print(self):
         print("In print of B")
 
 
-class C(B, A):
+class C(A):
     pass
 
 
 obj = C()
-obj.print()
-obj.process()
-
-print( isinstance(obj,C))
-print( isinstance(10,int))
-print( issubclass(int,object))
-
+# obj.print()
+print(C.mro())
